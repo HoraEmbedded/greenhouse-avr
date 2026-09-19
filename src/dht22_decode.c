588ab9cb490a2205e044633193b561ee81109f8a
HoraEmbeddded <azonhoumonhoracia@gmail.com>
2026-09-19 14:35:09 +0100

    Implement DHT22 checksum validation and temperature/humidity decode

--- src/dht22_decode.c
+++ src/dht22_decode.c
@@ -7,7 +7,6 @@ uint8_t dht22_checksum_valid(const uint8_t data[5]) {
 
 Dht22Reading dht22_decode(const uint8_t data[5]) {
     Dht22Reading r;
-
     r.air_humidity_decipercent = (uint16_t)((data[0] << 8) | data[1]);
 
     int16_t magnitude = (int16_t)(((data[2] & 0x7F) << 8) | data[3]);
