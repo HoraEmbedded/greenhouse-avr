644dfe5b55eb212233733d7e2a6051cb8ff0bc63
HoraEmbedded <azonhoumonhoracia@gmail.com>
2026-09-22 14:08:42 +0000

    feat(v2): define SensorData_t and ActuatorState_t

--- firmware_v2_rtos_iot/include/config.h
+++ firmware_v2_rtos_iot/include/config.h
@@ -1,7 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// --- Configuration des tâches ---
+#include <stdint.h>
+#include <stdbool.h>
+
 #define TASK_SENSORS_PRIORITY   3
 #define TASK_LOGIC_PRIORITY     2
 #define TASK_NETWORK_PRIORITY   1
@@ -10,24 +12,24 @@
 #define TASK_LOGIC_STACK        4096
 #define TASK_NETWORK_STACK      8192
 
-// --- Fréquences ---
 #define SENSOR_SAMPLE_FREQ_HZ   20
 #define SENSOR_SAMPLE_PERIOD_MS (1000 / SENSOR_SAMPLE_FREQ_HZ)
 
-// --- Seuils d'hystérésis ---
-#define FAN_ON_TEMP_C           26.0f
-#define FAN_OFF_TEMP_C          24.0f
-#define PUMP_ON_SOIL_PCT        30.0f
-#define PUMP_OFF_SOIL_PCT       60.0f
-
-// --- Structure de données capteurs ---
 typedef struct {
-    float temperature_c;
-    float humidity_pct;
-    float soil_moisture_pct;
-    bool  water_level_ok;
-    bool  is_daytime;
+    float    temperature_c;
+    float    humidity_pct;
+    float    soil_moisture_pct;
+    bool     water_level_ok;
+    bool     is_daytime;
     uint32_t timestamp_ms;
 } SensorData_t;
 
-#endif // CONFIG_H
+typedef struct {
+    bool     fan_on;
+    bool     pump_on;
+    bool     sensor_fault;
+    uint8_t  dht_failures;
+    uint32_t timestamp_ms;
+} ActuatorState_t;
+
+#endif
