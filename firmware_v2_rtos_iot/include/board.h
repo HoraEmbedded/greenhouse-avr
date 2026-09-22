48690cd5f5b6118d84df4ccab05fd41732339a94
HoraEmbedded <azonhoumonhoracia@gmail.com>
2026-09-22 15:00:50 +0000

    feat(v2): add esp32 board pin mapping

---
+++ firmware_v2_rtos_iot/include/board.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#define PIN_I2C_SDA      21
+#define PIN_I2C_SCL      22
+#define PIN_DHT22        4
+#define PIN_SOIL_ADC     34
+#define PIN_WATER_LEVEL  27
+#define PIN_FAN_LED      25
+#define PIN_PUMP_LED     26
+
+#define I2C_FREQ_HZ      100000
+#define ADC_RESOLUTION   10
+
+#endif
